59d641f5beb63b059dbec1c031f561b8ed8c0bd9
jerry9977 <jerryeileen6037@hotmail.com>
2022-08-04 23:22:09 +1000

    add static root

--- paraMassage/settings.py
+++ paraMassage/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = "/var/www/example.com/static/"
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
