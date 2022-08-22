e294c8db4f9e00317736b789a4f59a183f1a5f38
jerry9977 <jerryeileen6037@hotmail.com>
2022-08-23 00:59:12 +1000

    update asgi setup

--- paraMassage/asgi.py
+++ paraMassage/asgi.py
@@ -9,13 +9,15 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paraMassage.settings')
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 import main.routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paraMassage.settings')
 
 django_asgi_app = get_asgi_application()
 
