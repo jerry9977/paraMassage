45bd29b6be9cf440ab35d9ea9ca998a085d7b229
jerry9977 <jerryeileen6037@hotmail.com>
2022-08-29 22:45:24 +1000

    disable debug

--- paraMassage/settings.py
+++ paraMassage/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--qsy6lzawpve(7skc1s(x@d9=qvthr@vaa-n9r56yhdbjx5owv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
