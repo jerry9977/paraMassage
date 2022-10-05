bdbd5965dd84902e28c7c85880b0753d29c77caa
jerry9977 <jerryeileen6037@hotmail.com>
2022-10-05 15:06:47 +1000

    typo

--- main/views.py
+++ main/views.py
@@ -472,8 +472,8 @@ def upload_receipt(request):
         print("===========", flush=True)
         print("===========", flush=True)
         print("outside", flush=True)
-        print(request.FILE.get("image"), flush=True)
-        print(request.FILE, flush=True)
+        print(request.FILES.get("image"), flush=True)
+        print(request.FILES, flush=True)
         print("============", flush=True)
         if request.FILES.get("image", None) is not None:
             print("===========", flush=True)
